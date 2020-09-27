e160394d8f16b0c49f47dca000e65b5e9c55d7f3
Teodora Serbanescu <teo.serbanescu16@gmail.com>
2020-09-28 00:17:53 +0300

    Page fault handler: verify if address is in a VMA

--- kernel/vma/pfault.c
+++ kernel/vma/pfault.c
@@ -7,6 +7,13 @@
 int task_page_fault_handler(struct task *task, void *va, int flags)
 {
 	/* LAB 4: your code here. */
+	struct vma *vma;
+
+	vma = find_vma(NULL, NULL, &task->task_rb, va);
+	if (va < vma->vm_base || va > vma->vm_end) {
+	    return -1;
+	}
+
 	return populate_vma_range(task, ROUNDDOWN(va, PAGE_SIZE), PAGE_SIZE, flags);
 }
 
